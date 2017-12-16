a7b5932c100c24379dbc82a19d1277f77e58d0ca
dpen2000 <davidpendray@gmail.com>
2017-12-16 21:54:17 +0000

    Initial setup

--- GildedRose.Tests/InventoryManagerTests.cs
+++ GildedRose.Tests/InventoryManagerTests.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace GildedRose.Tests
 {
-    public class Class1
+    public class InventoryManagerTests
     {
+        [Fact]
+        public void Test1()
+        {
+
+        }
     }
 }
