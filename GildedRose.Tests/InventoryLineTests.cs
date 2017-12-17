1cd8ced3bd4cb234435ca166406738c671ab89fa
dpen2000 <davidpendray@gmail.com>
2017-12-17 00:19:26 +0000

    Refactor: Move InventoryLine class into own file

--- GildedRose.Tests/InventoryLineTests.cs
+++ GildedRose.Tests/InventoryLineTests.cs
@@ -66,43 +66,5 @@ namespace GildedRose.Tests
             inventoryLine.Quality.ShouldBe(2);
             inventoryLine.SellIn.ShouldBe(2);
         }
-
-        public class InventoryLine
-        {
-            public int Quality { get; set; }
-            public int SellIn { get; set; }
-            public string ItemName { get; set; }
-
-            internal void PerformDailyUpdate()
-            {
-                if (ItemName == "Sulfuras") return;
-
-                var previousQuality = Quality;
-                if (ItemName == "Aged Brie")
-                {
-                    Quality++;
-                }
-                else if (SellIn <= 0)
-                {
-                    Quality = Quality - 2;
-                }
-                else
-                {
-                    Quality--;
-                }
-
-                if (Quality < 0)
-                {
-                    Quality = previousQuality;
-                }
-
-                if (Quality > 50)
-                {
-                    Quality = 50;
-                }
-
-                SellIn--;
-            }
-        }
     }
 }
